80a1eb2e06d653cb8a645af38a0b15555322e00f
benhillcsu <benhill@rams.colostate.edu>
2018-11-04 16:09:40 -0700

    Add files via upload

---
+++ CppSocketWraper/chatClient.cpp
@@ -0,0 +1,89 @@
+#include <iostream> 
+#include "tcpClientSocket.h"
+#include <algorithm>
+#include <string>
+#include <ctime>
+#include <fstream>
+using namespace std; 
+
+void readHelpFile(string filePath){
+    string line;
+    ifstream file(filePath);
+    if(file.is_open()){
+        while(getline(file,line)){
+            cout << line << "\n";
+        }
+    }
+}
+
+
+int main(int argc, char ** argv)
+{
+    
+    string command;
+    cout << "Starting client example" <<endl; 
+    string serverIP(argv[1]);
+    string username = argv[2];
+    int port = atoi(argv[3]); 
+    //string serverIP("127.0.0.1");
+    //string username = "ben";
+    //int port = 2000;
+
+    cs457::tcpClientSocket clientSocket(serverIP,port);
+    int val = clientSocket.connectSocket(); 
+    cout << "Client Socket Value after connect = " << val << endl;
+    clientSocket.sendString("-u " + username, false);
+
+    //clientSocket.sendString("Hello Server. How are you? ",false); 
+    while(cin){
+        string command;
+        cout << "enter command: ";
+        cin >> command;
+        if(command == "/ISON"){
+            clientSocket.sendString(command, false);
+            string ison;
+            ssize_t c;
+            tie(ison,c) = clientSocket.recvString(4096,false);
+            cout << ison << '\n';
+        }
+        if(command == "PING"){
+            clock_t start;
+            start = clock();
+            clientSocket.sendString(command, false);
+            string pong;
+            ssize_t c;
+            tie(pong, c) = clientSocket.recvString(4096,false);
+            double timer = (clock() - start) / (double) CLOCKS_PER_SEC;
+            cout << "server has replied : " << pong << "\n" << "Server response time: " << timer << "\n";           
+        }if(command == "HELP"){
+            clientSocket.sendString(command, false);
+            string help;
+            ssize_t c;
+            tie(help, c) = clientSocket.recvString(4096,false);
+            readHelpFile(help);
+        }if(command == "TIME"){
+            clientSocket.sendString(command, false);
+            string serverTime;
+            ssize_t c;
+            tie(serverTime, c) = clientSocket.recvString(4096,false);
+            cout << serverTime << '\n';
+        }
+        
+    }
+
+    string msg;
+    ssize_t v;
+    tie(msg,v) =  clientSocket.recvString(4096,false);
+    cout << "server said: " << msg << endl;
+
+    cout << "Client will try to exit now" <<endl;
+
+    clientSocket.sendString("EXIT",false); 
+
+    tie(msg,v) =  clientSocket.recvString(4096,false);
+    cout << "server said: " << msg << endl;
+
+    clientSocket.closeSocket(); 
+    
+    return 0; 
+}
---
+++ CppSocketWraper/tcpClientSocket.cpp
@@ -0,0 +1,97 @@
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include "tcpClientSocket.h"
+#include <string> 
+#include <cstring> 
+#include <vector> 
+#include <iostream>
+using namespace std; 
+    
+
+cs457::tcpClientSocket::tcpClientSocket(string networkAddress, uint portNumber): address(networkAddress), port(portNumber)
+{
+    init(); 
+    setSocketOptions();
+};
+
+
+void cs457::tcpClientSocket::init()
+{
+    //here I may have differences with server 
+    //check after testing
+    clientSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    const char * cstr = address.c_str();
+    int val =0;  
+    bzero(&serverAddress,sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    val = inet_aton(cstr,&addr);
+    serverAddress.sin_addr = addr;
+    //string addresscpy(inet_ntoa(addr));
+    //address = addresscpy;  
+    serverAddress.sin_port = htons(port);
+}
+
+void cs457::tcpClientSocket::setSocketOptions()
+{
+    int optval = 1;
+    setsockopt(clientSocket, SOL_SOCKET, SO_REUSEADDR, 
+	                (const void *)&optval , sizeof(int));
+}
+
+int cs457::tcpClientSocket::connectSocket()
+{   
+    return connect(clientSocket,(struct sockaddr *)&serverAddress,sizeof(serverAddress));
+
+}
+
+int cs457::tcpClientSocket::closeSocket()
+{
+    return close(clientSocket);
+}
+
+std::tuple<string,ssize_t> cs457::tcpClientSocket::recvString(int bufferSize, bool useMutex)
+{
+    char stringBuffer[bufferSize]; 
+    //fixed by Ben
+    memset(stringBuffer, 0, sizeof(stringBuffer));    //change made here. Zeros out buffer.
+
+    ssize_t recvMsgSize;
+
+    if (useMutex)
+    {
+        lock_guard<mutex> lock(recvMutex);
+        recvMsgSize = recv(clientSocket, stringBuffer, bufferSize, 0); 
+    }    
+    else
+    {
+        recvMsgSize = recv(clientSocket, stringBuffer, bufferSize, 0); 
+    }
+    
+    
+   
+    return make_tuple(string(stringBuffer),recvMsgSize);     
+};
+        
+
+ssize_t cs457::tcpClientSocket::sendString(const string & data, bool useMutex)
+{
+    //https://stackoverflow.com/questions/7352099/stdstring-to-char
+    if (data.size() == 0) return 0;                 
+    int bufferSize = data.size()+1; 
+    vector<char> stringBuffer(data.c_str(), data.c_str() + data.size() + 1u);
+    //std::cout << data << "\n";
+
+    ssize_t rval; 
+    if (useMutex)
+    {
+       lock_guard<mutex> lock(sendMutex);
+       rval = send(clientSocket, &stringBuffer[0], bufferSize, 0);
+    }
+    else
+    {
+       rval = send(clientSocket, &stringBuffer[0], bufferSize, 0);    
+       
+    }
+
+    return rval;
+}
